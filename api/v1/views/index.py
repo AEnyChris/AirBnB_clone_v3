a89f6e4694eeef2ec122f5916a261c4817dc6a58
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-08-03 14:26:23 +0100

    count of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,20 @@
 from flask import jsonify
 from api.v1.views import app_views
 import json
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    return jsonify(
+            amenities=storage.count("Amenity"),
+            cities=storage.count("City"),
+            places=storage.count("Place"),
+            reviews=storage.count("Review"),
+            states=storage.count("State"),
+            users=storage.count("User"))
