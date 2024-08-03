42858b6cab8f1c821da5b16a1a71481a304a14cf
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-08-03 03:10:50 +0100

    first route of the api

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines the views status code 200"""
+
+from flask import jsonify
+from api.v1.views import app_views
+import json
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    return jsonify(status="OK")
